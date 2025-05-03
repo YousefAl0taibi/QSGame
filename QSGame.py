a1e02cca5bd5e433189116840c66ee18623e9a28
Yousef Alotaibi <442101224@student.ksu.edu.sa>
2025-05-03 11:36:40 +0300

    Third clean version of BST project

--- QSGame.py
+++ QSGame.py
@@ -1,29 +1,27 @@
 import csv
-import copy
-
-def generate_QS(filename, key_column):
-    attributes_values = {}
+def generate_QS(filename,key_column): # to generate the all posible questions
+    attributes_values={}
     with open(filename, newline='', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
+        reader=csv.DictReader(file) #read from file
         for row in reader:
-            for attr, val in row.items():
-                if attr != key_column  and not(val=="Female" or val=="No"):
+            for attr,val in row.items(): #take values to make program know what should do with eache qeustion
+                if attr!=key_column  and not(val=="Female" or val=="No"):
                     if attr not in attributes_values:
-                        attributes_values[attr] = set()  
+                        attributes_values[attr]=set()  
                     attributes_values[attr].add(val)
 
-    questions = {}
-    for attr, attr_value in attributes_values.items():
+    questions={}
+    for attr, attr_value in attributes_values.items(): #make the text question to show to the user
         for value in attr_value:
             if(value=="Yes" or value=="No"):
-                question = f"Is the person's {attr}?"
+                question=f"Is the person's {attr}?"
             else:
-                question = f"Is the person's {attr} {value}?"
-            questions[question] = [attr, value]
+                question=f"Is the person's {attr} {value}?"
+            questions[question]=[attr, value]
 
     return questions
 
-def Hashmap(filename,key_column):
+def Hashmap(filename,key_column):#save data in hashmap
     hashmap={}
     with open(filename,newline='',encoding='utf-8') as file:
         reader=csv.DictReader(file)
@@ -38,118 +36,109 @@ keycolumn='Name'
 hashmap=Hashmap(filename,keycolumn)
 Questions=generate_QS(filename,"Name")
 
-class Node:
+class Node:#make node to each person and each question
     def __init__(self,question=None,data=None):
         self.left=None
         self.right=None
         self.question=question
         self.data=data
 
-class BST:
-    def __init__(self, data: dict):
-        self.root = Node(None, data)
+class BST:#make BST to run every question and person inside it
+    def __init__(self,data:dict):
+        self.root = Node(None,data)
 
-    def insert(self, node, data: dict, question: str, answer:str, attr: str, attr_value: str):
+    def insert(self, node,data:dict,question:str,answer:str,attr:str,attr_value:str):#insert every data in left and right
         if node is None:
             return Node(question, data)
 
-        left_data = {key:val for key,val in data.items() if val.get(attr) == attr_value}
-        right_data = {key:val for key,val in data.items() if val.get(attr) != attr_value}
-        node.question = question
-        node.left = Node(question,data=left_data)
-        node.right = Node(question,data=right_data)
-        if answer == "Yes":
-            return node.left
+        left_data={key:val for key,val in data.items() if val.get(attr) == attr_value}
+        right_data={key:val for key,val in data.items() if val.get(attr) != attr_value}
+        node.question=question
+        node.left=Node(question,data=left_data)
+        node.right=Node(question,data=right_data)
+        if answer=="Yes":
+            return (node,node.left)
         
-        elif answer == "No":
-            return node.right
+        elif answer=="No":
+            return (node,node.right)
 
         return node
 
-    def build_tree(self, questions: dict, data: dict):
+    def build_tree(self,questions:dict):#do The game here
         print("\nWelcome to The Questions Game")
         print("\nAnswer The Questions with Yes or No to find the famous person")
         node = self.root
-        questions_copy = questions.copy()
-        current_data = node.data.copy()  
-        questions_asked = 0
+        questions_copy=questions.copy()
+        current_data=node.data.copy()  
+        questions_asked=0
 
-        while len(current_data) > 0:
-            qskey = self.best_QS(questions_copy, current_data)
+        while len(current_data)>1:
+            qskey=self.best_QS(questions_copy, current_data)#take the best question
             if qskey is None:
                 break
 
-            attr, attr_value = questions_copy[qskey]
+            attr,attr_value=questions_copy[qskey]
 
             print(f"\nQuestion: {qskey}")
-            user_input = input("\n(Yes/No)? \n").strip().capitalize()
+            user_input=input("\n(Yes/No)? \n").strip().capitalize()
 
             while user_input not in ["Yes", "No"]:
-               user_input = input("Please enter Yes or No: ").strip().capitalize()
+               user_input=input("Please enter Yes or No: ").strip().capitalize()
 
-            questions_asked += 1
+            questions_asked+=1
             
 
         
-            if user_input == "Yes":
-                current_data = {k:val for k,val in current_data.items() if val[attr] == attr_value}
+            if user_input=="Yes":
+                current_data={k:val for k,val in current_data.items() if val[attr]==attr_value}
 
-            elif user_input == "No":
-                current_data = {k:val for k,val in current_data.items() if val[attr] != attr_value}
+            elif user_input=="No":
+                current_data={k:val for k,val in current_data.items() if val[attr]!=attr_value}
 
         
-            questions_copy = {key: val for key, val in questions_copy.items() if key!=qskey}
+            questions_copy={key:val for key,val in questions_copy.items() if key!=qskey}
 
-            node = self.insert(node, node.data, qskey, user_input, attr, attr_value)
-            node.data = current_data
-            self.print_tree(node, user_input)
+            parent,node=self.insert(node,node.data,qskey,user_input,attr,attr_value) #insert data to the tree
+            node.data=current_data #change the pointer to the salect node
+            self.print_tree(parent,node)#print the how much remaining person in left and right
         print(f"The Number of Questions Needed={questions_asked}")
         print("Best Case:6 Questions")
         print("Worst Case:50 Questions")
             
             
-    def print_tree(self, node, answer:str):
+    def print_tree(self,node,current_node):
         if node is None:
             return
         
-        if len(node.data) == 1:
-            print("\n{"+f"Final Answer: {list(node.data.keys())[0]}"+"}\n")
+        if len(current_node.data)==1:
+            print("\n{"+f"Final Answer: {list(current_node.data.keys())[0]}"+"}\n")
             return
         
         else:
             print("remaining names:\n")
-
-            for name in node.data.keys():
-                print(f"({name})", end=" ")
+            print(f"Yes:{len(node.left.data)}", end=" ")
+            print(f"No:{len(node.right.data)}")
             print("\n")
 
         
 
-        if answer:
-            
-            if answer == "Yes":
-                self.print_tree(node.left, answer)
-            elif answer == "No":
-                self.print_tree(node.right, answer)
-
-    def best_QS(self,questions:dict,data:dict):
-        best_split = 1000
-        lst_bestQS = {}
+    def best_QS(self,questions:dict,data:dict):#take the best question
+        lst_bestQS={}
     
-        for qskey, (attr, attr_value) in questions.items():
-            left = sum(1 for d in data.values() if d[attr] == attr_value)
-            right = sum(1 for d in data.values() if d[attr] != attr_value)
-            abs_sum = abs(left - right)
+        for qskey,(attr,attr_value) in questions.items():
+            left=sum(1 for d in data.values() if d[attr]==attr_value)
+            right=sum(1 for d in data.values() if d[attr]!=attr_value)
+            abs_sum=abs(left-right)
 
       
-            if left > 0 and right > 0:
-                lst_bestQS[qskey] = abs_sum
+            if left>0 and right>0:#to take a valuable question
+                lst_bestQS[qskey]=abs_sum
 
     
         if not lst_bestQS:
             return None
 
-        bestQS = min(lst_bestQS, key=lst_bestQS.get)
+        bestQS=min(lst_bestQS, key=lst_bestQS.get)
         return bestQS
             
 
@@ -157,5 +146,5 @@ class BST:
 
 
 
-bst = BST(hashmap)
-bst.build_tree(Questions,hashmap)
+bst = BST(hashmap) #create the tree
+bst.build_tree(Questions)#start the game
